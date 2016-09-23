bf43dda72f28dcf568aedf27ce64ccd6ca1bd8b0
mattjburrows <mattjburrows@gmail.com>
2016-09-23 08:15:17 +0100

    removing jquery

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,12 +5,16 @@ define([
   function bind() {
     var $window = $(window);
     var $document = $(document);
-    var $jsToggleDropdown = $('.js-toggle-dropdown');
+    var jsToggleDropdown = document.querySelectorAll('.js-toggle-dropdown')[0];
     var $list = $('.main-nav__list');
+    var list = document.querySelectorAll('.main-nav__list')[0];
     var $dropdown = $('.main-nav__dropdown');
+    var dropdown = document.querySelectorAll('.main-nav__dropdown')[0];
     var $dropdownItems = $dropdown.find('.main-nav__dropdown__item');
-    var linkCount = ($dropdownItems.length - 1);
+    var dropdownItems = dropdown.querySelectorAll('.main-nav__dropdown__item');
+    var linkCount = (dropdownItems.length - 1);
     var $listToggleItem = $list.find('.main-nav__list__toggle');
+    var listToggleItem = list.querySelectorAll('.main-nav__list__toggle')[0];
     var listToggleItemWidth = $listToggleItem.outerWidth();
     var states = {
       expanded: 'is-expanded',
@@ -28,47 +32,59 @@ define([
     }, 150);
 
     function removeLinks() {
-      $list
-        .removeAttr('style')
-        .find('.' + states.clone)
-          .remove();
+      list.removeAttribute('style');
+      Array.prototype.forEach.call(list.querySelectorAll('.' + states.clone), function (listItem) {
+        listItem.parentNode.removeChild(listItem);
+      });
 
-      $dropdown
-        .find('.main-nav__nested')
-          .removeClass(states.expanded);
+      dropdown
+        .querySelectorAll('.main-nav__nested')[0]
+        .classList.remove(states.expanded);
 
-      $dropdownItems.removeClass(states.hidden);
+      Array.prototype.forEach.call(dropdownItems, function(dropdownItem) {
+        dropdownItem.classList.remove(states.hidden);
+      });
     }
 
     function toggleListToggleItem(listWidthExceeded, allItemsVisible) {
       if (!listWidthExceeded || allItemsVisible) {
-        $listToggleItem.addClass(states.hidden);
+        listToggleItem.classList.add(states.hidden);
       } else {
-        $listToggleItem.removeClass(states.hidden);
+        listToggleItem.classList.remove(states.hidden);
       }
     }
 
-    function buildNavigationItems() {
-      $listToggleItem.removeClass(states.hidden);
+    function outerWidth(el) {
+      var width = el.offsetWidth;
+      var style = getComputedStyle(el);
 
-      var itemsWidth = $listToggleItem.outerWidth(true);
-      var listWidth = $list.width();
+      width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+      return width;
+    }
+
+    function buildNavigationItems() {
+      listToggleItem.classList.remove(states.hidden);
+
+      var itemsWidth = outerWidth(listToggleItem);
+      var listWidth = list.outerWidth;
       var visibleItems = 0;
       var listWidthExceeded;
 
-      $dropdownItems.each(function cloneLinks(i, item) {
+      Array.prototype.forEach.call(dropdownItems, function cloneLinks(item, i) {
         visibleItems = i;
 
-        var $item = $(item);
-        var $itemInner = $($item.html());
-        var $listItem = $('<li>', listItemOptions).append($itemInner);
+        var itemInner = item.innerHTML;
+        var listItem = document.createElement('li');
 
-        $listItem.insertBefore($listToggleItem);
+        console.log(itemInner)
 
-        itemsWidth += $listItem.outerWidth(true);
+        $(listItem).insertBefore($listToggleItem);
+        listItem.innerHTML = itemInner;
+
+        itemsWidth += outerWidth(listItem);
         listWidthExceeded = (itemsWidth + listToggleItemWidth) >= listWidth;
 
-        $item.addClass(states.hidden);
+        item.classList.add(states.hidden);
 
         if (listWidthExceeded) {
           return false;
@@ -79,56 +95,65 @@ define([
     }
 
     function toggleDropdown() {
-      $dropdown.toggleClass(states.expanded);
+      dropdown.classList.toggle(states.expanded);
     }
 
-    function setExpandedDropdownClass($dropdown){
-      var $expandedNavigation = $('.main-nav__nested.' + states.expanded);
+    function setExpandedDropdownClass(dropdown) {
+      var expandedNavigation = document.querySelectorAll('.main-nav__nested.' + states.expanded)[0];
+      var removeExpandedClass = (expandedNavigation !== undefined) && (dropdown !== expandedNavigation);
 
-      if (!$dropdown.is($expandedNavigation)) {
-        $expandedNavigation.removeClass(states.expanded);
+      if (removeExpandedClass) {
+        expandedNavigation.classList.remove(states.expanded);
       }
-      $dropdown.toggleClass(states.expanded);
+      dropdown.classList.toggle(states.expanded);
     }
 
-    function setActiveButtonClass($btn) {
-      var $activeBtn = $('.main-nav__link.' + states.active);
+    function setActiveButtonClass(btn) {
+      var activeBtn = document.querySelectorAll('.main-nav__link.' + states.active)[0];
+      var removeActiveClass = (activeBtn !== undefined) && (btn !== activeBtn);
 
-      if(!$btn.is($activeBtn)) {
-        $activeBtn.removeClass(states.active);
+      if(removeActiveClass) {
+        activeBtn.classList.remove(states.active);
       }
-      $btn.toggleClass(states.active);
+      btn.classList.toggle(states.active);
     }
 
-    function positionNestedDropdown($btn, $dropdown, $list) {
-      var isClone = $btn.parent().hasClass(states.clone);
+    function positionNestedDropdown(btn, dropdown, $list) {
+      var isClone = btn.parentNode.classList.contains(states.clone);
+      var isDropdownExpanded = dropdown.classList.contains(states.expanded);
 
-      if (isClone && $dropdown.hasClass(states.expanded)) {
-        var paddingBottomValue = $dropdown.height();
-        $list.css('padding-bottom', paddingBottomValue);
+      if (isClone && isDropdownExpanded) {
+        var paddingBottomValue = dropdown.offsetHeight;
+        list.style.paddingBottom = paddingBottomValue + 'px';
       } else {
-        $list.removeAttr('style');
+        list.removeAttribute('style');
       }
     }
 
-    function toggleNestedDropdown() {
-      var $btn = $(this);
-      var $dropdown = $btn.siblings('.main-nav__nested');
-
-      setExpandedDropdownClass($dropdown);
-      setActiveButtonClass($btn);
-      positionNestedDropdown($btn, $dropdown, $list);
+    function getSiblings(element, selector) {
+      return Array.prototype.filter.call(element.parentNode.querySelectorAll(selector), function (child) {
+        return child !== element;
+      });
     }
 
-    $window
-      .off('resize.build-navigation-items')
-      .on('resize.build-navigation-items', windowResize);
+    function toggleNestedDropdown(event) {
+      var btn = event.target;
+      var dropdown = getSiblings(btn, '.main-nav__nested')[0];
+
+      setExpandedDropdownClass(dropdown);
+      setActiveButtonClass(btn);
+      positionNestedDropdown(btn, dropdown);
+    }
+
+    window.removeEventListener('resize', windowResize);
+    window.addEventListener('resize', windowResize);
+
     $document
       .off('click.toggle-sub-navigation')
       .on('click.toggle-sub-navigation', '.js-toggle-nested', toggleNestedDropdown);
-    $jsToggleDropdown
-      .off('click.toggle-navigation')
-      .on('click.toggle-navigation', toggleDropdown);
+
+    jsToggleDropdown.removeEventListener('click', toggleDropdown);
+    jsToggleDropdown.addEventListener('click', toggleDropdown);
 
     buildNavigationItems();
   }
